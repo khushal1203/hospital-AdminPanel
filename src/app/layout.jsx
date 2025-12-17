088279d103078724a08b8a2797dc82c8d98284e0
heena.2dots <heena.2dots@gmail.com>
2025-12-17 12:34:22 +0530

    frist push

---
+++ src/app/layout.jsx
@@ -0,0 +1,27 @@
+import "@/css/satoshi.css";
+import "@/css/style.css";
+
+import NextTopLoader from "nextjs-toploader";
+import { Providers } from "./providers";
+
+export const metadata = {
+  title: {
+    template: "%s | NextAdmin - Next.js Dashboard Kit",
+    default: "NextAdmin - Next.js Dashboard Kit",
+  },
+  description:
+    "Next.js admin dashboard toolkit with 200+ templates, UI components, and integrations for fast dashboard development.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <Providers>
+          <NextTopLoader color="#5750F1" showSpinner={false} />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
